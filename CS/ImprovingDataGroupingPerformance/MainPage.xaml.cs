898cbb2ac8d7baa808a0517101f74e5c08075ac9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ImprovingDataGroupingPerformance/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace ImprovingDataGroupingPerformance {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+            GroupData(new string[]{"Country", "City", "Quantity", "UnitPrice"});
+        }
+        private void GroupData(string[] fieldNames) {
+            grid.BeginDataUpdate();
+            try {
+                grid.ClearGrouping();
+                for (int i = 0; i < fieldNames.Length; i++) {
+                    GridColumn col = grid.Columns[fieldNames[i]];
+                    if (col != null)
+                        grid.GroupBy(col);
+                }
+            }
+            finally {
+                grid.EndDataUpdate();
+            }
+        }
+    }
+}
---
+++ VB/ImprovingDataGroupingPerformance/MainPage.xaml.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Grid
+
+Namespace ImprovingDataGroupingPerformance
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+			GroupData(New String(){"Country", "City", "Quantity", "UnitPrice"})
+		End Sub
+		Private Sub GroupData(ByVal fieldNames() As String)
+			grid.BeginDataUpdate()
+			Try
+				grid.ClearGrouping()
+				For i As Integer = 0 To fieldNames.Length - 1
+					Dim col As GridColumn = grid.Columns(fieldNames(i))
+					If col IsNot Nothing Then
+						grid.GroupBy(col)
+					End If
+				Next i
+			Finally
+				grid.EndDataUpdate()
+			End Try
+		End Sub
+	End Class
+End Namespace
